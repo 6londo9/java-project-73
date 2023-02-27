ef9095acb339d194baed1371c1127f8f5cfb51ba
Илья Остапенко <6londo9@MacBook-Pro-Ilya.local>
2023-02-27 21:42:11 +1100

    add User entity | Generate migrations

---
+++ app/src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,33 @@
+package hexlet.code.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @CreatedDate
+    private Instant createdAt;
+
+}

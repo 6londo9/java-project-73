b4d5f810438b287a60fbb3f20cbba45cff796cd6
Илья Остапенко <6londo9@MacBook-Pro-Ilya.local>
2023-05-09 14:15:03 +0300

    Try fix build

--- src/main/java/hexlet/code/controller/WelcomeController.java
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -12,7 +12,7 @@ import static com.rollbar.spring.webmvc.RollbarSpringConfigBuilder.withAccessTok
 @RestController
 public class WelcomeController {
 
-    @Value("${ROLLBAR_TOKEN:}")
+    @Value("${ROLLBAR_TOKEN}")
     private String rollbarToken;
     @Value("${spring.profiles.active:}")
     private String activeProfile;
